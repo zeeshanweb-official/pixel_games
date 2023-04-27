d56c3844c77e4b82ba17124aac8cbcfe07abc3e1
zeeshan <zeeshanweb.official@gmail.com>
2023-04-27 23:31:37 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+
+<template>
+  <div class="main">
+    <div class="bg-image min-h-screen min-w-full flex flex-col">
+      <div class="container mx-auto">this is a container</div>
+    </div>
+  </div>
+</template>
+
+<style>
+.bg-image {
+  background-image: url("./assets/bg.svg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+}
+</style>
