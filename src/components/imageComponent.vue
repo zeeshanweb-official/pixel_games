1e9c857d221004e5a86c4a277dbc8e2df55a0d6d
Mubashar <mubashariqbal902@gmail.com>
2023-04-28 06:27:30 +0500

    work done

---
+++ src/components/imageComponent.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    color: String
+});
+import imageGreen from '@/assets/SVG/Asset 1.svg'
+import imageRed from '@/assets/SVG/Asset 2.svg'
+
+</script>
+<template>
+    <img class="md:w-1/6 sm:w-2/12 w-1/12" :src="color === 'red' ? imageRed : imageGreen" alt="" />
+</template>
+
+  
---
+++ src/components/remaininLives.vue
@@ -0,0 +1,14 @@
+<script setup>
+import ImageComponent from './imageComponent.vue';
+const remaingLives = 2
+const totalLives=5
+</script>
+<template>
+    <ImageComponent v-for="(item,index) in totalLives" :key="index" :color="index<=remaingLives?'green':'red'" />
+    <!-- <img class="md:w-1/6 sm:w-2/12 w-1/12" src="@/assets/SVG/Asset 1.svg" alt="" />
+    <img class="md:w-1/6 sm:w-2/12 w-1/12" src="@/assets/SVG/Asset 4.svg" alt="" />
+    <img class="md:w-1/6 sm:w-2/12 w-1/12" src="@/assets/SVG/Asset 6.svg" alt="" />
+    <img class="md:w-1/6 sm:w-2/12 w-1/12" src="@/assets/SVG/Asset 5.svg" alt="" />
+    <img class="md:w-1/6 sm:w-2/12 w-1/12" src="@/assets/SVG/Asset 8.svg" alt="" /> -->
+</template>
+<style></style>
